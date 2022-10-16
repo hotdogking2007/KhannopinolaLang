4bf5b494653e4d4eca5ae5d5bd8fd57292f5aa65
hotdogking2007 <hotdogopman2@gmail.com>
2022-10-16 15:26:46 +0900

    lexer 추가

---
+++ lexer/lexer.go
@@ -0,0 +1,147 @@
+package lexer
+
+import (
+	"KhannopinolaLang/token"
+	"unicode"
+)
+
+type Lexer struct {
+	input        []rune
+	position     int
+	readPosition int
+	ch           rune
+}
+
+func New(input string) *Lexer {
+	l := &Lexer{input: []rune(input)}
+	l.readChar()
+	return l
+}
+
+func (l *Lexer) readChar() {
+	if l.readPosition >= len(l.input) {
+		l.ch = 0
+	} else {
+		l.ch = rune(l.input[l.readPosition])
+	}
+	l.position = l.readPosition
+	l.readPosition += 1
+}
+
+func (l *Lexer) NextToken() token.Token {
+	var tok token.Token
+
+	l.skipWhitesapce()
+	switch l.ch {
+	case rune('='):
+		if l.peekChar() == rune('=') {
+			ch := l.ch
+			l.readChar()
+			literal := string(ch) + string(ch)
+			tok = token.Token{Type: token.EQ, Literal: literal}
+		} else {
+			tok = newToken(token.ASSIGN, l.ch)
+		}
+	case rune('+'):
+		tok = newToken(token.PLUS, l.ch)
+	case rune('-'):
+		tok = newToken(token.MINUS, l.ch)
+	case rune('!'):
+		if l.peekChar() == '=' {
+			ch := l.ch
+			l.readChar()
+			literal := string(ch) + string(ch)
+			tok = token.Token{Type: token.EQ, Literal: literal}
+		} else {
+			tok = newToken(token.BANG, l.ch)
+		}
+	case rune('/'):
+		tok = newToken(token.SLASH, l.ch)
+	case rune('<'):
+		tok = newToken(token.LT, l.ch)
+	case rune('>'):
+		tok = newToken(token.GT, l.ch)
+	case rune(';'):
+		tok = newToken(token.SEMICOLON, l.ch)
+	case rune(','):
+		tok = newToken(token.COMMA, l.ch)
+	case rune('('):
+		tok = newToken(token.LPAREN, l.ch)
+	case rune(')'):
+		tok = newToken(token.RPAREN, l.ch)
+	case rune('{'):
+		tok = newToken(token.LBRACE, l.ch)
+	case rune('}'):
+		tok = newToken(token.RBRACE, l.ch)
+	case rune('"'):
+		tok.Type = token.STRING
+		tok.Literal = l.readString()
+	case rune(0):
+		tok.Literal = ""
+		tok.Type = token.EOF
+	default:
+		if unicode.IsLetter(l.ch) {
+			tok.Literal = l.readIdentifier()
+			tok.Type = token.LookupIdent(tok.Literal)
+			return tok
+		} else if isDigit(l.ch) {
+			tok.Type = token.INT
+			tok.Literal = l.readNumber()
+			return tok
+		} else {
+			tok = newToken(token.ILLEGAL, l.ch)
+		}
+	}
+
+	l.readChar()
+	return tok
+}
+func (l *Lexer) peekChar() rune {
+	if l.readPosition >= len(l.input) {
+		return rune(0)
+	} else {
+		return rune(l.input[l.readPosition])
+	}
+}
+func (l *Lexer) skipWhitesapce() {
+	for l.ch == ' ' || l.ch == '\t' || l.ch == '\n' || l.ch == '\r' {
+		l.readChar()
+	}
+}
+func (l *Lexer) readIdentifier() string {
+	position := l.position
+	if unicode.IsLetter(l.ch) {
+		l.readChar()
+		for unicode.IsLetter(l.ch) || unicode.IsNumber(l.ch) {
+			l.readChar()
+		}
+	}
+	return string(l.input[position:l.position])
+}
+
+func (l *Lexer) readNumber() string {
+	position := l.position
+	for isDigit(l.ch) {
+		l.readChar()
+	}
+	return string(l.input[position:l.position])
+}
+
+func (l *Lexer) readString() string {
+	position := l.position + 1
+	for {
+		l.readChar()
+		if l.ch == rune('"') || l.ch == rune(0) {
+			break
+		}
+	}
+
+	return string(l.input[position:l.position])
+}
+func isDigit(ch rune) bool {
+	return rune('0') <= ch && ch <= rune('9')
+}
+
+func newToken(tokenType token.TokenType, ch rune) token.Token {
+	return token.Token{Type: tokenType, Literal: string(ch)}
+}
---
+++ token/token.go
@@ -0,0 +1,65 @@
+package token
+
+type TokenType string
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	IDENT  = "IDENT"
+	INT    = "INT"
+	STRING = "STRING"
+
+	ASSIGN   = "="
+	PLUS     = "+"
+	MINUS    = "-"
+	BANG     = "!"
+	ASTERISK = "*"
+	SLASH    = "/"
+
+	LT     = "<"
+	GT     = ">"
+	EQ     = "=="
+	NOT_EQ = "!="
+
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+	TRUE     = "TRUE"
+	FALSE    = "FALSE"
+	IF       = "IF"
+	ELSE     = "ELSE"
+	RETURN   = "RETURN"
+	ASM      = "ASM"
+)
+
+var keywords = map[string]TokenType{
+	"func":   FUNCTION,
+	"fn":     FUNCTION,
+	"let":    LET,
+	"true":   TRUE,
+	"false":  FALSE,
+	"if":     IF,
+	"else":   ELSE,
+	"return": RETURN,
+	"asm":    ASM,
+}
+
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}
